3ce276a9d95df50c6c5c278b3f6fe00481281c84
Yejin.Shin <mingmi_27@naver.com>
2024-04-04 15:09:59 +0900

    first commit

---
+++ lib/prac01/birthday_card.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const BirthdayScreen(),
+    );
+  }
+}
+
+class BirthdayScreen extends StatelessWidget {
+  const BirthdayScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffE7BDB6),
+      body: Container(
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Positioned(
+              child: Background(),
+            ),
+            Positioned(left: 60, bottom: 80, child: LottieTest()),
+            Positioned(
+              top: 100,
+              left: 0,
+              right: 0,
+                child: BirthdayText(),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          //image: NetworkImage(
+          //    "https://img.freepik.com/free-photo/pastel-plastic-texture-background-holographic-gradient_53876-106193.jpg?t=st=1712133865~exp=1712137465~hmac=4ade3895f333230cfcdcbaf016d0c62647c5da8b46287b202655557579bd01be&w=740"),
+          image: AssetImage('assets/image/cake.jpeg'),
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
+
+class BirthdayText extends StatelessWidget {
+  const BirthdayText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Text(
+            'Happy Birthday',
+            style: TextStyle(
+              fontSize: 48,
+              color: Color(0xffB24648),
+            ),
+          ),
+        ),
+        Center(
+          child: Text(
+            'Yejin🥳',
+            style: TextStyle(
+              fontSize: 48,
+              fontWeight: FontWeight.w800,
+              color: Color(0xffB24648),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class LottieTest extends StatefulWidget {
+  @override
+  _LottieTestState createState() => _LottieTestState();
+}
+
+class _LottieTestState extends State<LottieTest> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Lottie.asset(
+        'assets/lottie/cake.json',
+        width: 400,
+        height: 400,
+      ),
+    );
+  }
+}
---
+++ lib/prac01/business_card.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const BusinessScreen(),
+    );
+  }
+}
+
+class BusinessScreen extends StatelessWidget {
+  const BusinessScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffffffff),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Background(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Transform.translate(
+                      offset: Offset(0, -70),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ProfileImage(),
+                          ProfileText(),
+                        ],
+                      )),
+                  Icon(Icons.edit),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 200,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(
+              "https://cdn.imweb.me/upload/S20210406ae69853331cfb/d24b50c47fb2e.jpg"),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileImage extends StatelessWidget {
+  const ProfileImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      backgroundColor: Colors.white,
+      radius: 64,
+      child: CircleAvatar(
+          backgroundImage: AssetImage('assets/image/profile.jpg'), radius: 60),
+    );
+  }
+}
+
+class ProfileText extends StatelessWidget {
+  const ProfileText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '신예진(Yejin Shin)',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+          ),
+          Text(
+            'Mobile Developer | Flutter',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            '대한민국 경기도 고양시',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            'https://github.com/Gunbam27',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: Color(0xff1b75cf),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LottieTest extends StatefulWidget {
+  @override
+  _LottieTestState createState() => _LottieTestState();
+}
+
+class _LottieTestState extends State<LottieTest> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Lottie.asset(
+        'assets/lottie/cake.json',
+        width: 400,
+        height: 400,
+      ),
+    );
+  }
+}
